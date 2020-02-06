eac569fab0ac8a3153e1511f68042926c26787a7
Tomasz Cudziło <tomasz@luzid.co>
2020-02-06 16:55:20 +0100

    Use example server from networking exercise that uses mongo

--- docker-compose/index.js
+++ docker-compose/index.js
@@ -1,7 +1,14 @@
-// more-or-less the example code from the hapi-pino repo
 const hapi = require("@hapi/hapi");
+const { MongoClient } = require("mongodb");
+const url = process.env.MONGO_CONNECTION_STRING || "mongodb://localhost:27017";
+const dbName = "dockerApp";
+const collectionName = "count";
 
 async function start() {
+  const client = await MongoClient.connect(url);
+  const db = client.db(dbName);
+  const collection = db.collection(collectionName);
+
   const server = hapi.server({
     host: "0.0.0.0",
     port: process.env.PORT || 3000
@@ -10,8 +17,18 @@ async function start() {
   server.route({
     method: "GET",
     path: "/",
-    handler() {
-      return { success: true };
+    async handler() {
+      const count = await collection.count();
+      return { success: true, count };
+    }
+  });
+
+  server.route({
+    method: "GET",
+    path: "/add",
+    async handler() {
+      const res = await collection.insertOne({});
+      return { inserted: res.insertedCount };
     }
   });
 
